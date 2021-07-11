012f8f0a1d6459f2fc1fced643aa75c300ef5bee
Михаил <televonvea@gmail.com>
2021-07-11 20:28:45 +0300

    www

---
+++ src/assets/js/app.js
@@ -0,0 +1,13 @@
+//= ../../../node_modules/aos/dist/aos.js
+// console.log(AOS)
+AOS.init();
+var options = {
+  "animate": true,
+  "patternWidth": 100,
+  "patternHeight": 74.84,
+  "grainOpacity": 0.06,
+  "grainDensity": 1,
+  "grainWidth": 1,
+  "grainHeight": 1
+}
+grained('#main', options);
